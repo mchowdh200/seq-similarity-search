00ffa525c7dc3f59680f5f3683d067fe25cab4f4
Murad <mchowdh200@gmail.com>
2022-05-25 13:16:37 -0600

    add index creation rule and cluster/sbatch files

---
+++ generate-index.smk
@@ -0,0 +1,45 @@
+import os
+from types import SimpleNamespace
+configfile: 'config/generate-index.yaml'
+config = SimpleNamespace(**config)
+
+
+rule All:
+    index = f'{config.outdir}/{config.index_name}',
+    id_map =  f'{config.outdir}/{config.index_name}.id_map.gz'
+
+rule CompileAsMac:
+    output:
+        'AsMac/_softnw.cpython-38-x86_64-linux-gnu.so'
+    conda:
+        'envs/asmac.yaml'
+    shell:
+        """
+        cd AsMac
+        python setup_softnw.py build_ext --inplace
+        """
+
+rule IndexFastqs:
+    ## TODO
+
+rule CreateIndex:
+    input:
+        asmac_compiled = rules.CompileAsMac.output,
+        seqs = config.seqs, # list of files
+    output:
+        index = f'{config.outdir}/{config.index_name}',
+        id_map =  f'{config.outdir}/{config.index_name}.id_map.gz'
+    conda:
+        'envs/asmac.yaml'
+    params:
+        gzipped = '--gzipped' if config.gzipped else ''
+    shell:
+        f"""
+        scripts/create_index.py \\
+        --model-weights AsMac/model/final.pt \\
+        --output {{output.index}} \\
+        --format {config.seq_filetype} \\
+        {{params.gzipped}} \\
+        --batch-size 64 \\
+        --seqs {{input.seqs}}
+        """
---
+++ scripts/create_index.py
@@ -0,0 +1,53 @@
+import argparse
+import gzip
+from collections import defaultdict
+
+from ..AsMac.AsMac_model import AsMac
+from ..AsMac.AsMac_utility import one_hot, SeqIteratorDataset
+
+import faiss
+import torch
+from torch.utils.data import DataLoader
+import numpy as np
+
+## Get args
+# =============================================================================
+parser = argparse.ArgumentParser()
+parser.add_argument('--model-weights', type=str, required=True, dest='weights',
+                    help='path to asmac model weights.')
+parser.add_argument('--output', type=str, required=True, dest='output',
+                    help='output path/name for faiss index.')
+parser.add_argument('--format', type=str, required=True, dest='format',
+                    help='seq file format -- fast{a|q}.')
+parser.add_argument('--gzipped', action='store_true', default=False,
+                    dest='gzipped', help='Is the file gzipped?')
+parser.add_argument('--batch-size', type=int, default=64, dest='batch_size',
+                    help='batch size for model to operate on.')
+parser.add_argument('--seqs', type=str, required=True, nargs='+', dest='seqs',
+                    help='list of seq file paths.')
+args = parser.parse_args()
+
+## load pretrained model
+# ==============================================================================
+alphabet_size = 4 # the model was trained with ATCG only
+embed_dim = 300 # number of kernel sequences
+kernel_size = 20 # kernel length
+net = AsMac(alphabet_size, embed_dim, kernel_size)
+net_state_dict = torch.load(args.model_weights)
+net.load_state_dict(net_state_dict)
+
+## get embeddings, add to faiss index
+# ==============================================================================
+dataset = SeqIteratorDataset(paths=args.seqs, format=args.format,
+                             gzipped=args.gzipped)
+dataloader = DataLoader(dataset=dataset, batch_size=args.batch_size)
+index = faiss.IndexFlatL2(embed_dim)
+with torch.no_grad(), gzip.open(f'{args.output}.id_map.gz', 'wt') as id_map:
+    for records in dataloader:
+        # add the embeddings (rows) to the index
+        seq_oh = one_hot(records['seq'])
+        embeddings = net.get_embeddings(seq_oh) \
+                        .detach().numpy().astype(np.float32)
+        index.add(embeddings) 
+        id_map.write(f'{records["index"]}\t{records["file"]}\t{records["id"]}\n')
+faiss.write_index(index, args.output)
