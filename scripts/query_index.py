0ab4a9b1950eebc29e97eb194777d7ef853aa575
Murad <mchowdh200@gmail.com>
2022-06-03 13:58:31 -0600

    add bed output to query script

--- scripts/query_index.py
+++ scripts/query_index.py
@@ -1,3 +1,4 @@
+from os.path import basename, dirname, splitext
 import sys
 import argparse
 from functools import partial
@@ -6,6 +7,7 @@ from multiprocessing import Pool
 import numpy as np
 import faiss
 from AsMac_utility import load_pretrained, SeqIteratorDataset, makeDataLoader
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--model-weights', type=str, required=True, dest='weights',
@@ -28,13 +30,15 @@ index: faiss.IndexFlatL2 = faiss.read_index(args.index)
 p = Pool(args.processes)
 
 for bed in args.beds:
-    dataset = SeqIteratorDataset(paths=bed, format='bed',
-                                 gzipped=False, alphabet='ATCG')
-    dataloader = makeDataLoader(dataset, batch_size=args.batch_size)
-    for batch in dataloader:
-        embeddings = np.array(p.map(partial(model.test_embed, asnumpy=True),
-                        [b['seq'] for b in batch]))
-        D, _, = index.search(embeddings, k=args.k)
-        print(D.shape, file=sys.stderr)
-        print(D[0])
-        print(D[1])
+    dir = dirname(bed)
+    sample = splitext(basename(bed))[0]
+    with open(f'{dir}/{sample}_scored.bed') as out:
+        dataset = SeqIteratorDataset(paths=[bed], format='bed',
+                                    gzipped=False, alphabet='ATCG')
+        dataloader = makeDataLoader(dataset, batch_size=args.batch_size)
+        for batch in dataloader:
+            embeddings = np.array(p.map(partial(model.test_embed, asnumpy=True),
+                            [b['seq'] for b in batch]))
+            D, _, = index.search(embeddings, k=args.k)
+            for i, b in enumerate(batch):
+                out.write(f"{b['interval']}\t{np.mean(D[i])}\n")
